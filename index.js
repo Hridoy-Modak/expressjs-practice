f01ff29d8c409fc873506cd26b67cbd0a76f78fc
Hridoy-Modak <hridoychandramodak@gmail.com>
2024-11-27 11:38:18 +0600

    Middleware start

--- index.js
+++ index.js
@@ -1,42 +1,60 @@
+// const express = require("express");
+// let app = express();
+// const EventEmitter = require("events");
+// let eventEmitter = new EventEmitter();
+
+// let count = 0;
+
+// eventEmitter.on("visit", () => {
+//   count++
+//   console.log(`Visitors count ${count} `);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("root directory");
+//   eventEmitter.emit("visit");
+// });
+
+// app.get("/about/:id", (req, res) => {
+//   res.send(`Directory is ${req.params.id} `);
+//   console.log(req.params.id);
+// });
+
+// app.get("/about", (req, res) => {
+//   // res.send("About")
+//   // console.log(typeof req.query.name);
+//   let{ name, id }=req.query;
+//   res.send({
+//     username:name,
+//     userid: id
+//   })
+// })
+
+// app.use((req,res)=>{
+//   res.send("not found")
+// })
+
+// app.listen(3000, () => {
+//   console.log("http://localhost:3000/ started");
+// });
+
 const express = require("express");
 let app = express();
-const EventEmitter = require("events");
-let eventEmitter = new EventEmitter();
+let port = 8000;
 
-let count = 0;
+function middlecheck(req, res, next) {
+  console.log("middleware function");
+  let result=true;
+  if(result){
 
-eventEmitter.on("visit", () => {
-  count++
-  console.log(`Visitors count ${count} `);
+  }
+  res.send("Success");
+}
+
+app.get("/", middlecheck, (req, res) => {
+  res.send({ msg: "user" });
 });
 
-
-app.get("/", (req, res) => {
-  res.send("root directory");
-  eventEmitter.emit("visit");
-});
-
-
-app.get("/about/:id", (req, res) => {
-  res.send(`Directory is ${req.params.id} `);
-  console.log(req.params.id);
-});
-
-
-app.get("/about", (req, res) => {
-  // res.send("About")
-  // console.log(typeof req.query.name);
-  let{ name, id }=req.query;
-  res.send({
-    username:name,
-    userid: id
-  })
-})
-
-app.use((req,res)=>{
-  res.send("not found")
-})
-
-app.listen(3000, () => {
-  console.log("http://localhost:3000/ started");
+app.listen(port || 5000, () => {
+  console.log(" server http://localhost:8000/ started");
 });
